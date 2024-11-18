844d7f9caf08addb8f57e3d14df25d8297f357b9
Bidyut Roy <bidyutroy2781@gmail.com>
2024-11-18 19:40:42 +0530

    adding the let info

--- l1.js
+++ l1.js
@@ -1,7 +1,7 @@
 // let learn about let,var and const
 
 const id=2378 //once a const variable is declared it can't be change 
-let name="bidyut roy" //let is a local scop variable
+let name="bidyut roy" //let is a local scop variable (new approach)
 var email="bidyutroy2781@gmail.com" //var is a global scope varibale
 
 console.table([id,name,email])
